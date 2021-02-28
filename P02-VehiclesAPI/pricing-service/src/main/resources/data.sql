6ffcc89bafd47f915ae1c8e389f888b21ad11954
prachjai <prachjai@adobe.com>
2021-02-28 18:32:05 +0530

    First commit

---
+++ P02-VehiclesAPI/pricing-service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO price (id, currency, price, vehicleId) VALUES (1, 'USD', 15000, 1);
+INSERT INTO price (id, currency, price, vehicleId) VALUES (2, 'USD', 20000, 2);
+INSERT INTO price (id, currency, price, vehicleId) VALUES (3, 'USD', 22000, 3);
+INSERT INTO price (id, currency, price, vehicleId) VALUES (4, 'USD', 28000, 4);
+INSERT INTO price (id, currency, price, vehicleId)) VALUES (5, 'USD', 18000, 5);
